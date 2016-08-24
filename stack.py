a21470d083d4252ba8b1ea677a353a94e98f22f4
nafay <manafay@gmail.com>
2016-08-24 23:47:08 +0530

    [Aug] [8-24] Data structure ListStack class

--- stack.py
+++ stack.py
@@ -2,12 +2,9 @@ class Stack():
 
     def __init__(self):
         self.__arr = []
-        self.__top = 0
 
     def push(self, elem):
-        self.__arr[self.__top] = elem
-        self.__top += 1
-        return self.__arr
+        self.__arr.append(elem)
 
     def pop(self):
         return self.__arr.pop()
@@ -15,13 +12,20 @@ class Stack():
     def is_empty(self):
         return len(self.__arr) == 0
 
+    def top(self):
+        return self.__arr[-1]
+
     def count(self):
         return len(self.__arr)
 
 
 s = Stack()
 
+print(s.push(5))
+print(s.push(3))
+print(s.push(5))
 print(s.push(7))
 print(s.pop())
 print(s.count())
 print(s.is_empty())
+print(s.top())
